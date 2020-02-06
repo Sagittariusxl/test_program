3e941d272726c601cd7b5e99e5fbbf522ca8bf7b
Евгений Леша <evgen.lesha@yandex.by>
2020-02-06 16:23:14 +0300

    jvfdvn

---
+++ test_program/Source.cpp
@@ -0,0 +1,13 @@
+#include "test_header.h"
+
+void test_function(void) {
+    int a, b, c;
+    
+    cout << "Enter b: ";
+    cin >> b;
+    cout << "n\Enter a: ";
+    cin >> c;
+    a = b + c;
+    cout << "n\Result: " << a;
+}
+ 
---
+++ test_program/test_header.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+void test_function(void);
